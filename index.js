321468d82f96b023ae392d262b4ab0a10740994d
Yaboisire <Yaboisire2gmail.com>
2023-07-28 10:21:50 +0300

    submit lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate")
+ {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity='go to the office')
+{
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(parameter1 = "*")
+ {
+    return function (parameter2 = "special") {
+        return `You are ${parameter1}${parameter2}${parameter1}!`
+    }
+}
+console.log(wrapAdjective("||")("a dedicated programmer"))
